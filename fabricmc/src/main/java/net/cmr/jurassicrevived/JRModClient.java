c0fd3c18df10cb8226c27c48e5af25c6963ef241
eligibbs <eligibbs@outlook.com>
2026-05-03 22:21:46 -0400

    fixes tranparency on tank block for fabric

--- fabricmc/src/main/java/net/cmr/jurassicrevived/JRModClient.java
+++ fabricmc/src/main/java/net/cmr/jurassicrevived/JRModClient.java
@@ -2,9 +2,16 @@ package net.cmr.jurassicrevived;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.cmr.jurassicrevived.block.ModBlocks;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
+
 public class JRModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		CommonClientClass.init();
+
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TANK.get(), RenderType.translucent());
 	}
 }
